d5fc1efa0614f2050575e5fb29dcee53ef5a38c8
Meysam Azad <MeysamAzad81@yahoo.com>
2018-12-11 18:07:30 +0330

    change db to postgresql

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': '$hayan!@#',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

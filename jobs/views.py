f466160c32ddb315cd23f48b8bce06186588ad72
Meysam Azad <MeysamAzad81@yahoo.com>
2018-12-11 20:05:29 +0330

    completed jobs, added image and summary to home_page

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
+from .models import Job
 # Create your views here.
 def index(request):
-    # return render(request, "home_page.html")
-    return TemplateResponse(request, 'jobs/home_page.html')
+    jobs = Job.objects
+    return TemplateResponse(request, 'jobs/home_page.html', {'jobs': jobs})

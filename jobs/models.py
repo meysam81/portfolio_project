e450261eb3b95f3ae50e2dec0883606943d44064
Meysam Azad <MeysamAzad81@yahoo.com>
2018-12-11 09:12:11 +0330

    add jobs model to admin page

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.db import models as db
 
 # Create your models here.
+class Job(db.Model):
+    image = db.ImageField(upload_to = 'images/')
+    summary = db.CharField(max_length = 200)

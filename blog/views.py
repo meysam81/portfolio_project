3011410fa860601752a4f5832bcef512210d6967
Meysam Azad <MeysamAzad81@yahoo.com>
2018-12-11 20:51:47 +0330

    add primary blog page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog
 
 # Create your views here.
+def index(request):
+    blogs = Blog.objects
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
